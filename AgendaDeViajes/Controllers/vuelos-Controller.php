1201cb523b8b7bb5c072cb3ebaac9c870fb1fbcb
MatiasVillegas23 <mati.villegas23@gmail.com>
2020-06-29 21:11:10 -0300

    agregar vuelo a viaje resuelto

--- AgendaDeViajes/Controllers/vuelos-Controller.php
+++ AgendaDeViajes/Controllers/vuelos-Controller.php
@@ -31,15 +31,15 @@ class VuelosController {
         $hora_salida = $_POST["hora_salida"];
         $fecha_llegada = $_POST["fecha_llegada"];
         $hora_llegada = $_POST["hora_llegada"];
-        //$id_viaje = $_POST["idViaje"]; 
-        //$viaje = $this->viajesModel->getViajeFechas($fecha_salida,$fecha_llegada);
-        //if (isset($viaje))  { //HACER VERIFICACIONES DE EN QUE FECHAS ESTA EL VUELO.
-            $id_viaje = 2;
-            //$id_viaje = $viaje->id_viaje;
-                    $this->vuelosModel->addVuelo($nro_vuelo,$compañia,$aeropuerto_origen,$aeropuerto_destino,$ciudad_origen,$ciudad_destino,$pais_origen,$pais_destino,$fecha_salida,$hora_salida,$fecha_llegada,$hora_llegada,$id_viaje);
-                    header("Location: " . HOME);
-            //    } //else 
-                   // $this->vuelosView->mostrarError("Las fechas no coinciden"); //MOSTRAR AGREGAR OTRO VUELO
+
+        $viaje = $this->viajesModel->getViajeFechas($fecha_salida,$fecha_llegada);
+        if (!is_null($viaje))  { //HACER VERIFICACIONES DE EN QUE FECHAS ESTA EL VUELO.
+            $miViaje = $viaje[0];
+            $id_viaje = (int)$miViaje->id_viaje;
+                $this->vuelosModel->addVuelo($nro_vuelo,$compañia,$aeropuerto_origen,$aeropuerto_destino,$ciudad_origen,$ciudad_destino,$pais_origen,$pais_destino,$fecha_salida,$hora_salida,$fecha_llegada,$hora_llegada,$id_viaje);
+                header("Location: " . HOME);
+            } //else
+                // $this->vuelosView->mostrarError("Las fechas no coinciden"); //MOSTRAR AGREGAR OTRO VUELO
     }
 
     public function addVueloMail () {
